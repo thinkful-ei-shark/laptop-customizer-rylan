5a1a3314acce46a64dbef859a5286217129ca045
Rylan Wassem <dryland28@gmail.com>
2020-10-02 20:56:39 -0400

    shopping list

--- src/ShoppingList/ShoppingList.js
+++ src/ShoppingList/ShoppingList.js
@@ -1,11 +1,90 @@
 import React from 'react';
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+import slugify from 'slugify';
 
-function ShoppingList(){
-    return(
-        <header>
-            <h1>Shopping List</h1>
-        </header>
-    );
+// This object will allow us to
+// easily convert numbers into US dollar values
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+const features = {
+    Processor: [
+      {
+        name: '17th Generation Intel Core HB (7 Core with donut spare)',
+        cost: 700
+      },
+      {
+        name: 'Professor X AMD Fire Breather with sidewinder technology',
+        cost: 1200
+      }
+    ],
+    'Operating System': [
+      {
+        name: 'Ubuntu Linux 16.04',
+        cost: 200
+      },
+      {
+        name: 'Bodhi Linux',
+        cost: 300
+      }
+    ],
+    'Video Card': [
+      {
+        name: 'Toyota Corolla 1.5v',
+        cost: 1150.98
+      },
+      {
+        name: 'Mind mild breeze 2000',
+        cost: 1345
+      }
+    ],
+    Display: [
+      {
+        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+        cost: 1500
+      },
+      {
+        name: '15.3" HGTV (3840 x 2160) Home makeover edition',
+        cost: 1400
+      }
+    ]
+  };
+
+function ShoppingList({selected, updateFeature}){
+    const shop_list = Object.keys(features).map((feature, idx) => {
+        const featureHash = feature + '-' + idx;
+        const options = features[feature].map(item => {
+          const itemHash = slugify(JSON.stringify(item));
+          return (
+            <div key={itemHash} className="feature__item">
+              <input
+                type="radio"
+                id={itemHash}
+                className="feature__option"
+                name={slugify(feature)}
+                checked={item.name === selected[feature].name}
+                onChange={e => updateFeature(feature, item)}
+              />
+              <label htmlFor={itemHash} className="feature__label">
+                {item.name} ({USCurrencyFormat.format(item.cost)})
+              </label>
+            </div>
+          );
+        });
+  
+        return (
+          <fieldset className="feature" key={featureHash}>
+            <legend className="feature__name">
+              <h3>{feature}</h3>
+            </legend>
+            {options}
+          </fieldset>
+        );
+      });
+    return shop_list;
 }
 
 export default ShoppingList;
