29699cea2b787761809c7b15a125790912e237aa
Rylan Wassem <dryland28@gmail.com>
2020-10-03 09:39:45 -0400

    Updates

---
+++ precache-manifest.374f3167893ea24531725d5a9c047b84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24c8c86171a19464bf9984bb6ed9b8a",
+    "url": "/laptop-customizer-rylan/index.html"
+  },
+  {
+    "revision": "2fc7ff30efb7d0135a7e",
+    "url": "/laptop-customizer-rylan/static/css/main.c8932f6a.chunk.css"
+  },
+  {
+    "revision": "c242cfc8b20b27aeb0c1",
+    "url": "/laptop-customizer-rylan/static/js/2.ad7e6bd9.chunk.js"
+  },
+  {
+    "revision": "2fc7ff30efb7d0135a7e",
+    "url": "/laptop-customizer-rylan/static/js/main.a2acf33f.chunk.js"
+  },
+  {
+    "revision": "1c5784e8ce30efa28b8c",
+    "url": "/laptop-customizer-rylan/static/js/runtime~main.9b1afedf.js"
+  }
+]);
